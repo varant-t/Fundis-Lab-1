ef3e6bf033f6a8b83107813a342a276740d71922
varant-t <vv_titizian@hotmail.ca>
2020-07-23 22:22:14 -0400

    Lab 1

---
+++ Lab 1/Assets/Scripts/Inventory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    LinkedList<int> my_list = new LinkedList<int>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        my_list.AddLast(1);
+        my_list.AddAfter(my_list.First, 2);
+
+        foreach (int i in my_list)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Bomb")
+        {
+            my_list.AddFirst(1);
+            Destroy(gameObject);
+            Debug.Log("In slot 1");
+        }
+
+        if (gameObject.tag == "Potion" && my_list.AddFirst(1) != null)
+        {
+            my_list.AddAfter(my_list.First, 2);
+            Destroy(gameObject);
+            Debug.Log("In slot 2");
+        }
+
+
+        if (gameObject.tag == "Ammo" && my_list.AddFirst(1) != null && my_list.AddAfter(my_list.First, 2) != null)
+        {
+            my_list.AddLast(3);
+            Destroy(gameObject);
+            Debug.Log("In slot 3");
+        }
+
+
+        /* if (gameObject.tag == "Bomb" || gameObject.tag == "Potion" || gameObject.tag == "Ammo")
+         {
+             my_list.AddFirst(1);
+             Debug.Log("In slot 1");
+         } 
+        else if(my_list.AddFirst(1))*/
+    }
+}
+
